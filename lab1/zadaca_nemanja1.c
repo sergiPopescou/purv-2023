ee1dfecd40d50378b31039bbf0c27ce0e0fd0c94
Nemanja Tripic <tripicnemanja96@gmail.com>
2023-03-17 15:13:48 +0100

    dodana modifikacija da bi se uklonila segmentation fault, tako sto se inicijalizuje promjenljiva s

---
+++ lab1/zadaca_nemanja1.c
@@ -0,0 +1,14 @@
+// pitanje sa stackoverflowa
+int main(void){
+      char s[50];
+      int ln;
+      puts("Enter String");
+      // scanf("%s", s);
+      gets(s);
+      ln = strlen(s); // remove this line to end seg fault
+      char *dyn_s = (char*) malloc (strlen(s)+1); //strlen(s) is used here as well but doesn't change outcome
+      dyn_s = s;
+      dyn_s[strlen(s)] = '\0';
+      puts(dyn_s);
+      return 0;
+    }
